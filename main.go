44a64ec8c5773ae3337c81945ade2c5791578006
Orantoon <71191565+Orantoon@users.noreply.github.com>
2021-10-16 02:23:42 -0600

    Intento de 1 y 2

--- main.go
+++ main.go
@@ -1,6 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"os"
+
+	"github.com/wcharczuk/go-chart"
+)
+
+/**
+1. Diseñar y construir una función que genere un arreglo de tamaño n con números enteros pseudo-aleatorios
+obtenidos mediante el método de congruencia lineal multiplicativa, a partir de una semilla dada. La semilla
+deberá ser un número primo entre 11 y 101. Los valores generados deben ser convertidos al intervalo 0 ..
+53. El período debe ser ≥ 2048. n puede ser cualquier número en el intervalo 200 .. 1000.
+*/
+
+func randArray(numSeed int64) []int {
+
+	rand.Seed(numSeed)
+
+	min := 200
+	max := 1000
+	n := rand.Intn(max - min + 1) + max
+	arr := make([]int, n)
+
+
+
+	return arr
+}
+
+/**
+2. Diseñar y construir una función que genere un gráfico de barras a partir de un arreglo de n números enteros,
+cada uno de los cuales pertenece al intervalo 0 .. 53. Las barras deben ser verticales y de tamaño
+directamente proporcional al número que representan.
+*/
+
+func arrChart() {
+	graph := chart.BarChart{
+		Title : "Gráfico de Barras",
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 30,
+			},
+		},
+		Height : 256,
+		BarWidth: 50,
+		Bars: []chart.Value{
+			{Value: 10, Label: "1"},
+			{Value: 11, Label: "2"},
+			{Value: 12, Label: "3"},
+			{Value: 13, Label: "4"},
+			{Value: 14, Label: "5"},
+			{Value: 15, Label: "6"},
+		},
+	}
+	f , _ := os.Create("resultados.png")
+	defer f.Close()
+	graph.Render(chart.PNG,f)
+}
 
 /**
 3. Diseñar y construir una función que haga la inserción de un valor entero (la llave) en un arreglo de
@@ -165,9 +221,11 @@ func binarySearchCall(arr []int, key int) (bool, int) {
 	return binarySearch(arr, key, 0, len(arr)-1, 1)
 }
 
+/*
 func main() {
 	arr := []int{1, 5, 10, 12, 16, 23, 29, 32, 35, 40, 49, 56, 57, 70} // in ascendent order
 
 	fmt.Println(binarySearchCall(arr, 56))
 	fmt.Println(binarySearchCall(arr, 24))
 }
+*/

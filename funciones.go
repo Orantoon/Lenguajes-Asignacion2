3d538a7f975e5390d25d93af7e387b7e38261480
Orantoon <71191565+Orantoon@users.noreply.github.com>
2021-10-17 23:51:15 -0600

    Funciones listas

--- funciones.go
+++ funciones.go
@@ -1,8 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-/**
+/*
+============
+1. Diseñar y construir una función que genere un arreglo de tamaño n con números enteros pseudo-aleatorios
+obtenidos mediante el método de congruencia lineal multiplicativa, a partir de una semilla dada. La semilla
+deberá ser un número primo entre 11 y 101. Los valores generados deben ser convertidos al intervalo 0 ..
+53. El período debe ser ≥ 2048. n puede ser cualquier número en el intervalo 200 .. 1000.
+*/
+
+func randArray(n int, seed int, k int, m int) []int {
+	
+	// Validating "n"
+	if n < 200 || n > 1000 {
+		fmt.Println("El valor n es incorrecto")
+		return nil
+	}
+	
+	// Validating Seed
+	if seed < 11 || seed > 101 {
+		fmt.Println("El valor de la semilla es incorrecto")
+		return nil
+	}
+
+	for i := 2; i < seed; i++ {	// Prime Number
+		if seed % i == 0 {
+			fmt.Println("El valor de la semilla es incorrecto")
+			return nil
+		}
+	}
+
+	// Validating "k"
+	if k < 0 {
+		fmt.Println("El valor k es incorrecto")
+		return nil
+	}
+
+	// Validating "m"
+	if m < 2048 {
+		fmt.Println("El valor m es incorrecto")
+		return nil
+	}
+	
+	arr := make([]int, n)
+	a := 8*k + 3	// 8k + 5 can also be used
+
+	for i := 0; i < n; i++ {	// Generating the Array
+		num := (a * seed) % m	// Main Algorithm, X = (a * [seed or previous number]) % m
+		arr[i] = num
+		seed = num	// Seed is now the previous number
+	}
+
+	//fmt.Println("Resultado: ", arr)
+
+	return arr
+}
+
+
+/*
+============
+2. Diseñar y construir una función que genere un gráfico de barras a partir de un arreglo de n números enteros,
+cada uno de los cuales pertenece al intervalo 0 .. 53. Las barras deben ser verticales y de tamaño
+directamente proporcional al número que representan.
+*/
+
+
+
+/*
+============
 3. Diseñar y construir una función que haga la inserción de un valor entero (la llave) en un arreglo de
 números enteros. Si la llave no se encuentra en el arreglo, inserta la llave al final del arreglo. Si la llave ya
 se encuentra en el arreglo, no la inserta. La función retorna un número entero, que será la cantidad de
@@ -20,7 +88,7 @@ func insertVal(arr *[]int, key int) int {
 	return len // Not found, checked all the values in the original array/slice and because it wasn't, it is the length of it.
 }
 
-//
+/*
 func test3(){
 	slice1, slice2 := []int{-12,32,99,67,-83,123}, []int{0,1,2,3,4}
 
@@ -29,9 +97,11 @@ func test3(){
 
 	fmt.Printf("Array 1 = Key: %d, Comparisons done: %d, Final Slice: %v\nArray 2 = Key: %d, Comparisons done: %d, Final Slice: %v", n1, three1, slice1, n2, three2, slice2)
 }
-//
+*/
 
-/**
+
+/*
+============
 4. Diseñar y construir una función que ordene ascendentemente un arreglo de enteros mediante el método de
 ordenamiento de selección. Este algoritmo es cuadrático en promedio.
 */
@@ -56,15 +126,17 @@ func selectionSort(arr *[]int) {
 	*arr = arr2 // Assign changes to original array
 }
 
-//
+/*
 func test4(){
 	arr := []int{-12,32,99,67,-83,123}
 	selectionSort(&arr)
 	fmt.Println(arr)
 }
-//
+*/
 
-/**
+
+/*
+============
 5. Diseñar y construir una función que ordene ascendentemente un arreglo de enteros mediante el método de
 ordenamiento conocido como Quicksort. Este algoritmo es O(n log2 (n)) en promedio.
 */
@@ -102,15 +174,17 @@ func quicksortCall(arr *[]int) {
 	quicksort(arr, 0, len(*arr)-1)
 }
 
-//
+/*
 func test5(){
 	arr := []int{-12,32,99,67,-83,123}
 	quicksortCall(&arr)
 	fmt.Println(arr)
 }
-//
+*/
 
-/**
+
+/*
+============
 6. Diseñar y construir una función que haga la búsqueda de un valor entero (la llave) en un arreglo que no
 esté ordenado, mediante el algoritmo de búsqueda secuencial. La función debe retornar un par ordenado:
 un valor booleano que indique si encontró la llave buscada (true = la encontró, false = no la encontró),
@@ -127,15 +201,17 @@ func linearSearch(arr []int, key int) (bool, int) {
 	return false, len(arr) // Not Found, the whole length of the array
 }
 
-//
+/*
 func test6(){
 	arr := []int{-12,32,99,67,-83,123}
 	fmt.Println(linearSearch(arr, 99))
 	fmt.Println(linearSearch(arr, 13))
 }
-//
+*/
 
-/**
+
+/*
+============
 7. Diseñar y construir una función que haga la búsqueda de un valor entero (la llave) en un arreglo que esté
 ordenado, mediante el algoritmo de búsqueda binaria. La función debe retornar un par ordenado: un valor
 booleano que indique si encontró la llave buscada (true = la encontró, false = no la encontró), junto con un
@@ -164,16 +240,18 @@ func binarySearchCall(arr []int, key int) (bool, int) {
 	return binarySearch(arr, key, 0, len(arr)-1, 1)
 }
 
-//
+/*
 func test7() {
 	arr := []int{1, 5, 10, 12, 16, 23, 29, 32, 35, 40, 49, 56, 57, 70} // in ascendent order
 
 	fmt.Println(binarySearchCall(arr, 56))
 	fmt.Println(binarySearchCall(arr, 24))
 }
-//
+*/
 
-/**
+
+/*
+============
 8. Diseñar una representación en Go para los árboles binarios de búsqueda ordinarios, donde cada nodo 
 almacene un número entero.
 */
@@ -197,7 +275,9 @@ func (n *Node) print() {
 	}
 }
 
-/**
+
+/*
+============
 9. Diseñar y construir una función que haga la inserción de un valor entero (la llave) en un árbol binario de 
 búsqueda ordinario. Si la llave no se encuentra en el árbol, crea un nuevo nodo con la llave. Si la llave ya
 se encuentra en el árbol, no la inserta. La función retorna un número entero, que será la cantidad de 
@@ -225,7 +305,9 @@ func (b *BSTree) insert(key int) int{
 	return b.count
 }
 
-/**
+
+/*
+============
 10. Diseñar y construir una función que haga la búsqueda de un valor entero (la llave) en un árbol binario de 
 búsqueda ordinario, sin hacer inserciones. La función debe retornar un par ordenado: un valor booleano 
 que indique si encontró la llave buscada (true = la encontró, false = no la encontró), junto con un entero 
@@ -252,7 +334,7 @@ func (b *BSTree) search(key int) (bool, int){
 	return b.searchNode(key, b.root), b.count
 }
 
-//
+/*
 func testBSTree(){
 	var b *BSTree = new(BSTree) //var b BSTree //
 
@@ -274,9 +356,10 @@ func testBSTree(){
 
 	fmt.Println("Search: ", a)
 }
-//
+*/
 
 
+/*
 func main(){
-	testBSTree()
-}
+	randArray(500, 19, 1, 2048)
+}*/
